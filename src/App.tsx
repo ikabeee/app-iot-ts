fb7bbada52c8f4b7757a461cb8611b43875ee018
ikabeee <carlglz30@gmail.com>
2025-03-12 22:22:45 -0500

    wrap Routes in HeroUIProvider to integrate navigation and enhance routing capabilities

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,28 @@
-import { Routes, Route } from "react-router"
+import { Routes, Route, useNavigate, NavigateOptions, useHref } from "react-router"
 import Dashboard from "./pages/Dashboard"
 import LayoutAdmin from "./layout/LayoutAdmin"
 import Login from "./pages/auth/Login"
 import Register from "./pages/auth/Register"
+import { HeroUIProvider } from "@heroui/system"
+declare module "@react-types/shared" {
+  interface RouterConfig {
+    routerOptions: NavigateOptions;
+  }
+}
 function App() {
-
-
+  const navigate = useNavigate();
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="register" element={<Register />} />
-      {/* Admin Routes */}
-      <Route path="admin" element={<LayoutAdmin />}>
-        <Route index element={<Dashboard />} />
-      </Route>
-    </Routes>
+    <HeroUIProvider navigate={navigate} useHref={useHref}>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        {/* Admin Routes */}
+        <Route path="admin" element={<LayoutAdmin />}>
+          <Route index element={<Dashboard />} />
+        </Route>
+      </Routes>
+    </HeroUIProvider>
+
   )
 }
 
