290f57191ae9cb3cc1887ba75cf997c893739a34
ikabeee <carlglz30@gmail.com>
2025-03-13 00:16:58 -0500

    refactor Dashboard component to include Card layout and fix MeasureCard import

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,11 +1,21 @@
+import { Card, CardBody, CardHeader } from "@heroui/card";
 import Map from "../components/Map";
-import MesureCard from "../components/MesureCard";
+import MeasureCard from "../components/MeasureCard";
 
 export default function Dashboard() {
     return (
-        <>
-            <Map />
-            <MesureCard />
-        </>
+        <main>
+            <Card className="py-4 shadow-small w-full h-[500px]">
+                <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+                    <h2 className="font-bold text-large">Tus sensores 📡</h2>
+                </CardHeader>
+                <CardBody className="overflow-hidden py-2">
+                    <Map className="w-full h-[100%] rounded-xl overflow-hidden" />
+                </CardBody>
+            </Card>
+            <div className="flex">
+                <MeasureCard />
+            </div>
+        </main>
     )
 }
