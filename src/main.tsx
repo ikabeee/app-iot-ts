c4a546572a8f70654a5ce852261a754ab7cae50c
ikabeee <carlglz30@gmail.com>
2025-03-12 22:22:50 -0500

    remove HeroUIProvider wrapper from main application entry point

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
-import { HeroUIProvider } from '@heroui/system'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <HeroUIProvider>
         <App />
-      </HeroUIProvider>
     </BrowserRouter>
   </StrictMode>,
 )
