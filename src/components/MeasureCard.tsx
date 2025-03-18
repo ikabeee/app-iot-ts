d00dcabd54d7fbe2e72d4f52836327fc278cb6cc
ikabeee <carlglz30@gmail.com>
2025-03-18 07:39:16 -0500

    refactor MeasureCard component to use @heroui/card and add props for title, value, and icon

--- src/components/MeasureCard.tsx
+++ src/components/MeasureCard.tsx
@@ -1,6 +1,22 @@
-export default function MeasureCard(){
+import { Card, CardBody, CardHeader } from "@heroui/card"
+interface MeasureCardProps{
+    className?: string,
+    title?: string,
+    value?: string,
+    icon?: React.ComponentType<React.SVGProps<SVGSVGElement>>,
+}
+
+export default function MeasureCard({className, title, value, icon:Icon}: MeasureCardProps){
     return (
-        <>
-        </>
+        <Card className={`${className}`}>
+            <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+                <h2 className="font-bold text-[18px] self-center">{title}</h2>
+            </CardHeader>
+            <CardBody className="flex">
+            {Icon && <Icon className="h-[100px] self-center" color="#AE7EDE" width={200} height={150}/>}
+                <p className="self-center text-[14px]">{value}</p>
+            </CardBody>
+        </Card>
+
     )
 }
