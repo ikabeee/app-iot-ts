3817d397105596de07e526def7faeb747296c518
ikabeee <carlglz30@gmail.com>
2025-03-18 07:39:08 -0500

    replace LoginForm component with AuthForm and update Login page layout

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,6 +1,9 @@
+import LoginForm from "../../components/AuthForm";
+
 export default function Login(){
     return (
-        <main className="bg-[#F1F2F7]">
+        <main className=" flex flex-col bg-[#F1F2F7] h-screen w-screen">
+            <LoginForm/>
             
         </main>
     )
